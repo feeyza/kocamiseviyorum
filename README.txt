b5b7e5f23657d982e5bbb032337f01009247c0a7
Feyza <129684598+feeyza@users.noreply.github.com>
2025-06-04 22:31:38 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,2 @@
+Bu klasöre 'i_wanna_be_yours.mp3' dosyasını eklemelisin.
+Şarkıyı telif haklarına dikkat ederek eklemelisin.
---
+++ script.js
@@ -0,0 +1,5 @@
+
+function nextLevel() {
+    alert("Bir sonraki level için hazır ol! 💫 
+(Devam sayfalarını ekleyebilirsin!)");
+}
